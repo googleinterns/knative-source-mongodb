1dbb6beac067d80a47c5ed9f636e4af1b419f3d8
selim5 <selim.hassairi@gmail.com>
2020-07-04 18:53:33 -0400

    watching changes

--- experimental/db-connection/main.go
+++ experimental/db-connection/main.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -28,12 +29,30 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+// Function to watch for changes
+func iterateChangeStream(routineCtx context.Context, waitGroup sync.WaitGroup, stream *mongo.ChangeStream) {
+	// Close watch stream and close the thread in waitgroup when done
+	defer stream.Close(routineCtx)
+	defer waitGroup.Done()
+	// For each new change recorded
+	for stream.Next(routineCtx) {
+		var data bson.M
+		if err := stream.Decode(&data); err != nil {
+			panic(err)
+		}
+		fmt.Printf("%v\n", data)
+
+	}
+}
+
 func main() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://10.12.0.13:27017"))
+	// Connect to the mongo replica-set
+	URI := "mongodb://10.12.0.12:27017" // Add IP:Port of main replica set pod
+	client, err := mongo.NewClient(options.Client().ApplyURI(URI))
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 20*time.Second)
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
@@ -43,9 +62,32 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Find the Databases and prints them
 	databases, err := client.ListDatabaseNames(ctx, bson.M{})
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(databases)
+
+	// Retrieve a specific collection
+	collection := client.Database("main-db").Collection("collection1")
+
+	// Watch for changes
+	// Create a wait group to be able to watch asynchronously
+	var waitGroup sync.WaitGroup
+
+	// Create a watch stream
+	collectionStream, err := collection.Watch(ctx, mongo.Pipeline{})
+	if err != nil {
+		panic(err)
+	}
+
+	// Add a thread to the wait group
+	waitGroup.Add(1)
+	// Create Routine context
+	routineCtx, _ := context.WithCancel(context.Background())
+	// Run the asynch routine
+	go iterateChangeStream(routineCtx, waitGroup, collectionStream)
+	// Keep running it in the backgorund and wait until its done
+	waitGroup.Wait()
 }
