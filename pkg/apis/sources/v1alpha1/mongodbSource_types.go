79adaa27c2dd7b6c64e544e0c99a041a51e0619d
selim5 <selim.hassairi@gmail.com>
2020-07-09 11:56:49 -0400

    prelimenary mongodb_types

--- pkg/apis/sources/v1alpha1/mongodbSource_types.go
+++ pkg/apis/sources/v1alpha1/mongodbSource_types.go
@@ -21,93 +21,87 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"knative.dev/pkg/apis"
 	"knative.dev/pkg/apis/duck"
 	duckv1 "knative.dev/pkg/apis/duck/v1"
 	"knative.dev/pkg/kmeta"
+	"knative.dev/pkg/webhook/resourcesemantics"
 )
 
 // +genclient
 // +genreconciler
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// MongodbSource is the Schema for the githubsources API
+// MongoDbSource is the Schema for the githubsources API
 // +k8s:openapi-gen=true
-type MongodbSource struct {
+type MongoDbSource struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   MongodbSourceSpec   `json:"spec,omitempty"`
-	Status MongodbSourceStatus `json:"status,omitempty"`
+	Spec   MongoDbSourceSpec   `json:"spec"`
+	Status MongoDbSourceStatus `json:"status,omitempty"`
 }
 
-// Check that CouchDb source can be validated and can be defaulted.
-var _ runtime.Object = (*MongodbSource)(nil)
-
-// Check that we can create OwnerReferences to a Configuration.
-var _ kmeta.OwnerRefable = (*MongodbSource)(nil)
-
-// Check that the type conforms to the duck Knative Resource shape.
-var _ duckv1.KRShaped = (*MongodbSource)(nil)
-
-// Check that MongodbSource implements the Conditions duck type.
-var _ = duck.VerifyType(&MongodbSource{}, &duckv1.Conditions{})
-
-// FeedType is the type of Feed
-type FeedType string
-
-var MongodbSourceEventTypes = []string{
-	MongodbSourceUpdateEventType,
-	MongodbSourceDeleteEventType,
+// GetGroupVersionKind returns the GroupVersionKind.
+func (s *MongoDbSource) GetGroupVersionKind() schema.GroupVersionKind {
+	return SchemeGroupVersion.WithKind("MongoDbSource")
 }
 
-const (
-	// MongodbSourceUpdateEventType is the MongodbSource CloudEvent type for update.
-	MongodbSourceUpdateEventType = "org.apache.couchdb.document.update"
-
-	// MongodbSourceDeleteEventType is the MongodbSource CloudEvent type for deletion.
-	MongodbSourceDeleteEventType = "org.apache.couchdb.document.delete"
-
-	// FeedNormal corresponds to the "normal" feed. The connection to the server
-	// is closed after reporting changes.
-	FeedNormal = FeedType("normal")
-
-	// FeedContinuous corresponds to the "continuous" feed. The connection to the
-	// server stays open after reporting changes.
-	FeedContinuous = FeedType("continuous")
+var (
+	// Check that MongoDbSource can be validated and defaulted.
+	_ apis.Validatable = (*MongoDbSource)(nil)
+	_ apis.Defaultable = (*MongoDbSource)(nil)
+	// Check that we can create OwnerReferences to a MongoDbSource.
+	_ kmeta.OwnerRefable = (*MongoDbSource)(nil)
+	// Check that MongoDbSource is a runtime.Object.
+	_ runtime.Object = (*MongoDbSource)(nil)
+	// Check that MongoDbSource satisfies resourcesemantics.GenericCRD.
+	_ resourcesemantics.GenericCRD = (*MongoDbSource)(nil)
+	// Check that MongoDbSource implements the Conditions duck type.
+	_ = duck.VerifyType(&MongoDbSource{}, &duckv1.Conditions{})
+	// Check that the type conforms to the duck Knative Resource shape.
+	_ duckv1.KRShaped = (*MongoDbSource)(nil)
+	// MongoDbSourceEventTypes represents the different Event Types
+	MongoDbSourceEventTypes = []string{
+		MongoDbSourceUpdateEventType,
+		MongoDbSourceDeleteEventType,
+	}
 )
 
-// MongodbSourceSpec defines the desired state of MongodbSource
-type MongodbSourceSpec struct {
+const (
+	// MongoDbSourceUpdateEventType is the MongoDbSource CloudEvent type for update.
+	MongoDbSourceUpdateEventType = ""
+
+	// MongoDbSourceDeleteEventType is the MongoDbSource CloudEvent type for deletion.
+	MongoDbSourceDeleteEventType = ""
+)
+
+// MongoDbSourceSpec defines the desired state of MongoDbSource
+type MongoDbSourceSpec struct {
 	// ServiceAccountName holds the name of the Kubernetes service account
 	// as which the underlying K8s resources should be run. If unspecified
 	// this will default to the "default" service account for the namespace
-	// in which the MongodbSource exists.
+	// in which the MongoDbSource exists.
 	// +optional
 	ServiceAccountName string `json:"serviceAccountName,omitempty"`
 
-	// CouchDbCredentials is the credential to use to access CouchDb.
+	// MongoDbCredentials is the credential to use to access MongoDb.
 	// Must be a secret. Only Name and Namespace are used.
-	CouchDbCredentials corev1.ObjectReference `json:"credentials,omitempty"`
-
-	// Feed changes how CouchDB sends the response.
-	// More information: https://docs.couchdb.org/en/stable/api/database/changes.html#changes-feeds
-	Feed FeedType `json:"feed"`
+	MongoDbCredentials corev1.ObjectReference `json:"credentials,omitempty"`
 
 	// Database is the database to watch for changes
 	Database string `json:"database"`
 
-	// Sink is a reference to an object that will resolve to a domain name to use as the sink.
+	// Collection is the collection to watch for changes
 	// +optional
-	Sink *duckv1.Destination `json:"sink,omitempty"`
+	Collection string `json:"collection,omitempty"`
+
+	// SourceSpec
+	duckv1.SourceSpec `json:",inline"`
 }
 
-// GetGroupVersionKind returns the GroupVersionKind.
-func (s *MongodbSource) GetGroupVersionKind() schema.GroupVersionKind {
-	return SchemeGroupVersion.WithKind("MongodbSource")
-}
-
-// MongodbSourceStatus defines the observed state of MongodbSource
-type MongodbSourceStatus struct {
+// MongoDbSourceStatus defines the observed state of MongoDbSource
+type MongoDbSourceStatus struct {
 	// inherits duck/v1 SourceStatus, which currently provides:
 	// * ObservedGeneration - the 'Generation' of the Service that was last
 	//   processed by the controller.
@@ -120,14 +114,14 @@ type MongodbSourceStatus struct {
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// MongodbSourceList contains a list of MongodbSource
-type MongodbSourceList struct {
+// MongoDbSourceList contains a list of MongoDbSource
+type MongoDbSourceList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []MongodbSource `json:"items"`
+	Items           []MongoDbSource `json:"items"`
 }
 
 // GetStatus retrieves the duck status for this resource. Implements the KRShaped interface.
-func (c *MongodbSource) GetStatus() *duckv1.Status {
+func (c *MongoDbSource) GetStatus() *duckv1.Status {
 	return &c.Status.Status
 }
