49cb89de69fe28fa00024b0ea0ed274833ff2424
selim5 <selim.hassairi@gmail.com>
2020-07-09 14:20:24 -0400

    prelimenary mongodbsource files

---
+++ pkg/apis/sources/v1alpha1/mongoDbSource_validation.go
@@ -0,0 +1,53 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"context"
+
+	"knative.dev/pkg/apis"
+)
+
+// Validate validates MongoDbSource
+func (m *MongoDbSource) Validate(ctx context.Context) *apis.FieldError {
+	var errs *apis.FieldError
+
+	//validation for "spec" field.
+	errs = errs.Also(m.Spec.Validate(ctx).ViaField("spec"))
+
+	//errs is nil if everything is fine.
+	return errs
+}
+
+// Validate validates MongoDbSourceSpecs
+func (ms *MongoDbSourceSpec) Validate(ctx context.Context) *apis.FieldError {
+	var errs *apis.FieldError
+
+	// Validate sink
+	if ms.Sink == nil {
+		fe := apis.ErrMissingField("sink")
+		errs = errs.Also(fe)
+	} else if fe := ms.Sink.Validate(ctx); fe != nil {
+		errs = errs.Also(fe.ViaField("sink"))
+	}
+
+	//Validation for serviceAccountName field.
+	if ms.ServiceAccountName == "" {
+		errs = errs.Also(apis.ErrMissingField("serviceAccountName"))
+	}
+	return errs
+}
