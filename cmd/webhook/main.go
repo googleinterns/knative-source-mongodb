da7a6180b0cfbbce06a6daa545f4f0d4c3b3dba1
selim5 <selim.hassairi@gmail.com>
2020-07-16 13:06:55 -0400

    preliminary webhook go file

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -16,6 +16,90 @@ limitations under the License.
 
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
 
+	mongodbv1alpha1 "github.com/googleinterns/knative-source-mongodb/pkg/apis/sources/v1alpha1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"knative.dev/pkg/configmap"
+	"knative.dev/pkg/controller"
+	"knative.dev/pkg/injection/sharedmain"
+	"knative.dev/pkg/signals"
+	"knative.dev/pkg/system"
+	"knative.dev/pkg/webhook"
+	"knative.dev/pkg/webhook/certificates"
+	"knative.dev/pkg/webhook/resourcesemantics"
+	"knative.dev/pkg/webhook/resourcesemantics/defaulting"
+	"knative.dev/pkg/webhook/resourcesemantics/validation"
+)
+
+var types = map[schema.GroupVersionKind]resourcesemantics.GenericCRD{
+	mongodbv1alpha1.SchemeGroupVersion.WithKind("MongoDbSource"): &mongodbv1alpha1.MongoDbSource{},
+}
+
+var callbacks = map[schema.GroupVersionKind]validation.Callback{}
+
+const admissionWebhookName = "mongodbsource-webhook"
+
+// NewDefaultingAdmissionController sets up mutating webhook.
+func NewDefaultingAdmissionController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+	return defaulting.NewAdmissionController(ctx,
+		// Name of the resource webhook.
+		fmt.Sprintf("defaulting.webhook.%s.sources.google.com", system.Namespace()),
+
+		// The path on which to serve the webhook.
+		"/defaulting",
+
+		// The resources to default.
+		types,
+
+		// A function that infuses the context passed to Validate/SetDefaults with custom metadata.
+		func(ctx context.Context) context.Context {
+			return ctx
+		},
+
+		// Whether to disallow unknown fields.
+		true,
+	)
+}
+
+// NewValidationAdmissionController sets up validation webhook.
+func NewValidationAdmissionController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+	return validation.NewAdmissionController(ctx,
+		// Name of the resource webhook.
+		fmt.Sprintf("defaulting.webhook.%s.sources.google.com", system.Namespace()),
+
+		// The path on which to serve the webhook.
+		"/validation",
+
+		// The resources to validate.
+		types,
+
+		// A function that infuses the context passed to Validate/SetDefaults with custom metadata.
+		func(ctx context.Context) context.Context {
+			return ctx
+		},
+
+		// Whether to disallow unknown fields.
+		true,
+
+		// Extra validating callbacks to be applied to resources.
+		callbacks,
+	)
+}
+
+func main() {
+	// Set up a signal context with our webhook options
+	ctx := webhook.WithOptions(signals.NewContext(), webhook.Options{
+		ServiceName: admissionWebhookName,
+		Port:        8443,
+		SecretName:  "webhook-certs",
+	})
+
+	sharedmain.WebhookMainWithContext(ctx, admissionWebhookName,
+		certificates.NewController,
+		NewDefaultingAdmissionController,
+		NewValidationAdmissionController,
+	)
 }
