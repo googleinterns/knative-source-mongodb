85ad143d5b0e0ae82f15d68df9983952c74010cd
selim5 <selim.hassairi@gmail.com>
2020-07-17 18:10:39 -0400

    preliminary file for controller

--- pkg/reconciler/mongodb/controller.go
+++ pkg/reconciler/mongodb/controller.go
@@ -15,3 +15,69 @@ limitations under the License.
 */
 
 package mongodb
+
+import (
+	"context"
+
+	"github.com/googleinterns/knative-source-mongodb/pkg/apis/sources/v1alpha1"
+	"github.com/googleinterns/knative-source-mongodb/pkg/reconciler"
+	"github.com/kelseyhightower/envconfig"
+	"k8s.io/client-go/tools/cache"
+
+	"knative.dev/pkg/configmap"
+	"knative.dev/pkg/controller"
+	"knative.dev/pkg/logging"
+
+	eventingclient "knative.dev/eventing/pkg/client/injection/client"
+	sinkbindinginformer "knative.dev/eventing/pkg/client/injection/informers/sources/v1alpha2/sinkbinding"
+	reconcilersource "knative.dev/eventing/pkg/reconciler/source"
+	kubeclient "knative.dev/pkg/client/injection/kube/client"
+	deploymentinformer "knative.dev/pkg/client/injection/kube/informers/apps/v1/deployment"
+
+	mongodbinformer "github.com/googleinterns/knative-source-mongodb/pkg/client/injection/informers/sources/v1alpha1/mongodbsource"
+	"github.com/googleinterns/knative-source-mongodb/pkg/client/injection/reconciler/sources/v1alpha1/mongodbsource"
+)
+
+const (
+	// ReconcilerName is the name of the reconciler
+	ReconcilerName = "MongoDbSource"
+)
+
+// NewController initializes the controller and is called by the generated code
+// Registers event handlers to enqueue events
+func NewController(
+	ctx context.Context,
+	cmw configmap.Watcher,
+) *controller.Impl {
+	deploymentInformer := deploymentinformer.Get(ctx)
+	sinkBindingInformer := sinkbindinginformer.Get(ctx)
+	mongodbSourceInformer := mongodbinformer.Get(ctx)
+
+	r := &Reconciler{
+		dr:  &reconciler.DeploymentReconciler{KubeClientSet: kubeclient.Get(ctx)},
+		sbr: &reconciler.SinkBindingReconciler{EventingClientSet: eventingclient.Get(ctx)},
+		// Config accessor takes care of tracing/config/logging config propagation to the receive adapter
+		configAccessor: reconcilersource.WatchConfigurations(ctx, "mongodb-source", cmw),
+	}
+	if err := envconfig.Process("", r); err != nil {
+		logging.FromContext(ctx).Panicf("required environment variable is not defined: %v", err)
+	}
+
+	impl := mongodbsource.NewImpl(ctx, r)
+
+	logging.FromContext(ctx).Info("Setting up event handlers")
+
+	mongodbSourceInformer.Informer().AddEventHandler(controller.HandleAll(impl.Enqueue))
+
+	deploymentInformer.Informer().AddEventHandler(cache.FilteringResourceEventHandler{
+		FilterFunc: controller.FilterGroupKind(v1alpha1.Kind("MongoDbSource")),
+		Handler:    controller.HandleAll(impl.EnqueueControllerOf),
+	})
+
+	sinkBindingInformer.Informer().AddEventHandler(cache.FilteringResourceEventHandler{
+		FilterFunc: controller.FilterGroupKind(v1alpha1.Kind("MongoDbSource")),
+		Handler:    controller.HandleAll(impl.EnqueueControllerOf),
+	})
+
+	return impl
+}
