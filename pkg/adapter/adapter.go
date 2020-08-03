8d92703a1b0705161f1b1d7a4beff27d30e090f9
selim5 <selim.hassairi@gmail.com>
2020-08-03 14:11:48 -0400

    final nacho comment fixes

--- pkg/adapter/adapter.go
+++ pkg/adapter/adapter.go
@@ -36,18 +36,16 @@ type envConfig struct {
 	MongoDbCredentialsPath string `envconfig:"MONGODB_CREDENTIALS" required:"true"`
 	Database               string `envconfig:"MONGODB_DATABASE" required:"true"`
 	Collection             string `envconfig:"MONGODB_COLLECTION" required:"false"`
-	EventSource            string `envconfig:"EVENT_SOURCE" required:"false"`
-	SinkURL                string `envconfig:"K_SINK" required:"false"`
+	EventSource            string `envconfig:"EVENT_SOURCE" required:"true"`
 }
 
 type mongoDbAdapter struct {
 	namespace       string
-	ce              cloudevents.Client
+	ceclient        cloudevents.Client
 	source          string
 	database        string
 	collection      string
 	credentialspath string
-	sinkurl         string
 	logger          *zap.SugaredLogger
 }
 
@@ -69,12 +67,11 @@ func NewAdapter(ctx context.Context, processed adapter.EnvConfigAccessor, ceClie
 
 	return &mongoDbAdapter{
 		namespace:       env.Namespace,
-		ce:              ceClient,
+		ceclient:        ceClient,
 		source:          env.EventSource,
 		database:        env.Database,
 		collection:      env.Collection,
 		credentialspath: env.MongoDbCredentialsPath,
-		sinkurl:         env.SinkURL,
 		logger:          logger,
 	}
 }
@@ -96,34 +93,32 @@ func (a *mongoDbAdapter) Start(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("Error creating mongo client: %w", err)
 	}
-	// Get dataSource
-	dataSource := getDataSource(client, a.database, a.collection)
+
+	// Get dataSource: either a mongo.Collection or a mongo.Database.
+	var dataSource dataSource
+	if a.collection != "" {
+		dataSource = client.Database(a.database).Collection(a.collection)
+	} else {
+		dataSource = client.Database(a.database)
+	}
 
 	// Connect to Client.
 	err = client.Connect(ctx)
 	if err != nil {
-		return fmt.Errorf("Error connecting to database: %s", err)
+		return fmt.Errorf("Error connecting to database: %w", err)
 	}
 	defer client.Disconnect(ctx)
 
 	// Create a watch stream for either the database or collection.
 	stream, err := dataSource.Watch(ctx, mongo.Pipeline{})
 	if err != nil {
-		return fmt.Errorf("Error setting up changeStream: %s", err)
+		return fmt.Errorf("Error setting up changeStream: %w", err)
 	}
 	defer stream.Close(ctx)
 
 	// Watch and process changes.
 	a.processChanges(ctx, stream)
 	return nil
-}
-
-// Returns the appropriate dataSource: database or collection.
-func getDataSource(client *mongo.Client, db string, coll string) dataSource {
-	if coll != "" {
-		return client.Database(db).Collection(coll)
-	}
-	return client.Database(db)
 }
 
 // processChanges processes the new incoming change, creates a cloud event and sends it.
@@ -133,18 +128,17 @@ func (a *mongoDbAdapter) processChanges(ctx context.Context, stream *mongo.Chang
 		var data bson.M
 		if err := stream.Decode(&data); err != nil {
 			a.logger.Desugar().Error("Error decoding the change stream", zap.Error(err))
+			continue
 		}
 		// Create corresponding event.
-		event, err := makeCloudEvent(data)
+		event, err := a.makeCloudEvent(data)
 		if err != nil {
 			a.logger.Desugar().Error("Failed to create event", zap.Error(err))
+			continue
 		}
 
-		// Send event using client.
-		ctx := cloudevents.ContextWithTarget(ctx, a.sinkurl)
-
 		// Send that Event.
-		if result := a.ce.Send(ctx, *event); cloudevents.IsUndelivered(result) {
+		if result := a.ceclient.Send(ctx, *event); cloudevents.IsUndelivered(result) {
 			a.logger.Desugar().Error("Failed to send event", zap.Any("result", result))
 		}
 	}
@@ -152,7 +146,7 @@ func (a *mongoDbAdapter) processChanges(ctx context.Context, stream *mongo.Chang
 }
 
 // makeCloudEvent makes a cloud event out of the change object recevied.
-func makeCloudEvent(data bson.M) (*cloudevents.Event, error) {
+func (a *mongoDbAdapter) makeCloudEvent(data bson.M) (*cloudevents.Event, error) {
 	// Create Event.
 	event := cloudevents.NewEvent(cloudevents.VersionV1)
 
@@ -163,7 +157,8 @@ func makeCloudEvent(data bson.M) (*cloudevents.Event, error) {
 	//		Data   -> data payload containing either id only for
 	//                deletion or full object for other changes.
 	event.SetID(data["_id"].(bson.M)["_data"].(string))
-	event.SetSource(data["ns"].(bson.M)["db"].(string) + "/" + data["ns"].(bson.M)["coll"].(string))
+	event.SetSource(a.source)
+	// event.SetSource(data["ns"].(bson.M)["db"].(string) + "/" + data["ns"].(bson.M)["coll"].(string))
 	event.SetType(data["operationType"].(string))
 
 	// Add payload if replace or insert, else add document key.
