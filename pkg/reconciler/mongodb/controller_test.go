385378694d9dff8ebb644a2d2bdd43d09f16007a
Selim Hassairi <selim.hassairi@gmail.com>
2020-08-06 22:01:03 -0400

    Controller UT (#53)
    * ctrler test + update codegen
    
    * fixed

---
+++ pkg/reconciler/mongodb/controller_test.go
@@ -0,0 +1,77 @@
+/*
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package mongodbsource
+
+import (
+	"os"
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"knative.dev/pkg/client/injection/ducks/duck/v1/addressable"
+	"knative.dev/pkg/configmap"
+	logtesting "knative.dev/pkg/logging/testing"
+	. "knative.dev/pkg/reconciler/testing"
+
+	// Fake injection informers
+	_ "github.com/googleinterns/knative-source-mongodb/pkg/client/clientset/versioned/typed/sources/v1alpha1/fake"
+	_ "github.com/googleinterns/knative-source-mongodb/pkg/client/injection/client/fake"
+	_ "github.com/googleinterns/knative-source-mongodb/pkg/client/injection/informers/sources/v1alpha1/mongodbsource/fake"
+	_ "github.com/googleinterns/knative-source-mongodb/pkg/reconciler/testing"
+	_ "knative.dev/eventing/pkg/reconciler/source"
+	_ "knative.dev/pkg/client/injection/kube/informers/apps/v1/deployment/fake"
+	_ "knative.dev/pkg/client/injection/kube/informers/core/v1/secret/fake"
+)
+
+func TestNew(t *testing.T) {
+	defer logtesting.ClearAll()
+	ctx, _ := SetupFakeContext(t)
+	ctx = addressable.WithDuck(ctx)
+	os.Setenv("MONGODB_RA_IMAGE", "example")
+	os.Setenv("METRICS_DOMAIN", "example")
+	cmw := configmap.NewStaticWatcher(&corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "config-observability",
+			Namespace: "google-sources",
+		},
+		Data: map[string]string{
+			"_example": "test-config",
+		},
+	}, &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "config-logging",
+			Namespace: "google-sources",
+		},
+		Data: map[string]string{
+			"zap-logger-config":   "test-config",
+			"loglevel.controller": "info",
+			"loglevel.webhook":    "info",
+		},
+	}, &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "config-tracing",
+			Namespace: "google-sources",
+		},
+		Data: map[string]string{
+			"_example": "test-config",
+		},
+	})
+
+	c := NewController(ctx, cmw)
+	if c == nil {
+		t.Fatal("Expected NewController to return a non-nil value")
+	}
+}
